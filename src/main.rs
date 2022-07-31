e4812c95dc952a599899563d937ce49acdfd3ca1
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-07-31 16:40:55 +0300

    update README.md

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 use std::io::stdin;
-use std::io::{self, BufRead, Write};
+use std::io::{self, Write};
 use std::process::Command;
 use std::thread::sleep;
-use std::time::{Duration, Instant};
+use std::time::{Duration};
 extern crate colour;
 
 fn cntdown(duration: u16) -> String {
@@ -27,76 +27,103 @@ fn start_timer(mut duration: u16) {
     }
     sleep(Duration::from_secs(1));
     print!("\r{}   \n", cntdown(duration));
+    io::stdout().flush().unwrap();
+    //println!();
 }
 
 fn check_continue(arg: String) {
-    println!("> Do you want to start your {} [Y/n]?  ", arg);
+    print!("> Do you want to start your {} [Y/n]?  ", arg);
+    io::stdout().flush().unwrap();
     let mut command = String::new();
     stdin()
         .read_line(&mut command)
         .ok()
         .expect("Failed to read line");
+    
+    while command.as_str() != "n\n" && command.as_str() != "y\n" && command.as_str() != "Y\n" {
+        print!("> Do you want to start your {} [Y/n]?  ", arg);
+        io::stdout().flush().unwrap();
+        stdin()
+            .read_line(&mut command)
+            .ok()
+            .expect("Failed to read line");
+    }
+
+    println!();
     if command.as_str() == "n\n" {
         colour::blue_ln!("> Take some extra time:");
         start_timer(4);
-        let notify = Command::new("notify-send")
+        let _notify = Command::new("notify-send")
             .arg("Extra time complete.")
             .output()
             .expect("failed to execute process");
-        let bepp = Command::new("aplay")
+        let _bepp = Command::new("aplay")
             .arg("beep.wav")
             .output()
             .expect("failed to notify");
-        println!("");
+        println!();
     }
 }
 
-fn main() {
-    let sessions: u16 = 4;
-    let work: u16 = 5; //45 * 60;
-    let short: u16 = 3; //5 * 60;
-    let long: u16 = 5; //15 * 60;
-
+fn run(sessions: u16, work: u16, short: u16, long: u16) {
     for session in 1..=sessions {
         colour::green_ln!("> Session {}/{}:", session, sessions);
         start_timer(work);
-        let notify = Command::new("notify-send")
+        let _notify = Command::new("notify-send")
             .arg("Work session complete. Take a break.")
             .output()
             .expect("failed to execute process");
-        let bepp = Command::new("aplay")
+        let _bepp = Command::new("aplay")
             .arg("beep.wav")
             .output()
             .expect("failed to notify");
-
-        println!(" ");
+        println!();
         check_continue("break".to_string());
 
         if session != sessions {
             colour::yellow_ln!("> Take a short break:");
             start_timer(short);
-            println!("");
-            let notify = Command::new("notify-send")
+            let _notify = Command::new("notify-send")
                 .arg("Break complete. Time to work.")
                 .output()
                 .expect("failed to execute process");
-            let bepp = Command::new("aplay")
+            let _bepp = Command::new("aplay")
                 .arg("beep.wav")
                 .output()
                 .expect("failed to notify");
+            println!();
             check_continue("work".to_string());
         } else {
             colour::cyan_ln!("> Take a long break:");
             start_timer(long);
-
-            let notify = Command::new("notify-send")
+            let _notify = Command::new("notify-send")
                 .arg("Pomodoro complete! Nice work!")
                 .output()
                 .expect("failed to execute process");
-            let bepp = Command::new("aplay")
+            let _bepp = Command::new("aplay")
                 .arg("beep.wav")
                 .output()
                 .expect("failed to notify");
         }
     }
+}
+
+fn main() {
+    // Default parameters
+    let mut sessions: u16 = 4;
+    let mut work: u16 = 45 * 60;
+    let mut short: u16 = 8 * 60;
+    let mut long: u16 = 20 * 60;
+
+    // target ses 4 w 45 s 5 l 5
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 9 {
+        sessions = args[2].to_string().parse::<u16>().unwrap(); 
+        work = 60 * args[4].to_string().parse::<u16>().unwrap();
+        short = 60 * args[6].to_string().parse::<u16>().unwrap();
+        long = 60 * args[8].to_string().parse::<u16>().unwrap();
+    }
+
+    run(sessions, work, short, long); 
 }
