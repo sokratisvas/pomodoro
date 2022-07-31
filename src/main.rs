6fd53b3debc8adc7f8f49a808c0229ab603d69e3
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-07-31 12:59:01 +0300

    add colours

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::{self, BufRead, Write};
 use std::process::Command;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+extern crate colour;
 
 fn cntdown(duration: u16) -> String {
     let hours: u16 = duration / 3600;
@@ -31,22 +32,33 @@ fn start_timer(mut duration: u16) {
 fn check_continue(arg: String) {
     println!("> Do you want to start your {} [Y/n]?  ", arg);
     let mut command = String::new();
-    stdin().read_line(&mut command)
-           .ok()
-           .expect("Failed to read line");
+    stdin()
+        .read_line(&mut command)
+        .ok()
+        .expect("Failed to read line");
     if command.as_str() == "n\n" {
+        colour::blue_ln!("> Take some extra time:");
         start_timer(4);
+        let notify = Command::new("notify-send")
+            .arg("Extra time complete.")
+            .output()
+            .expect("failed to execute process");
+        let bepp = Command::new("aplay")
+            .arg("beep.wav")
+            .output()
+            .expect("failed to notify");
+        println!("");
     }
 }
 
 fn main() {
     let sessions: u16 = 4;
-    let work: u16 = 45 * 60;
-    let short: u16 = 5 * 60;
-    let long: u16 = 15 * 60;
+    let work: u16 = 5; //45 * 60;
+    let short: u16 = 3; //5 * 60;
+    let long: u16 = 5; //15 * 60;
 
     for session in 1..=sessions {
-        println!("> Session {}/{}:", session, sessions);
+        colour::green_ln!("> Session {}/{}:", session, sessions);
         start_timer(work);
         let notify = Command::new("notify-send")
             .arg("Work session complete. Take a break.")
@@ -59,24 +71,32 @@ fn main() {
 
         println!(" ");
         check_continue("break".to_string());
-        
+
         if session != sessions {
-            println!("> Take a short break:");
+            colour::yellow_ln!("> Take a short break:");
             start_timer(short);
             println!("");
+            let notify = Command::new("notify-send")
+                .arg("Break complete. Time to work.")
+                .output()
+                .expect("failed to execute process");
+            let bepp = Command::new("aplay")
+                .arg("beep.wav")
+                .output()
+                .expect("failed to notify");
             check_continue("work".to_string());
         } else {
-            println!("> Take a long break:");
+            colour::cyan_ln!("> Take a long break:");
             start_timer(long);
-        }
 
-        let notify = Command::new("notify-send")
-            .arg("Work session complete. Take a break.")
-            .output()
-            .expect("failed to execute process");
-        let bepp = Command::new("aplay")
-            .arg("beep.wav")
-            .output()
-            .expect("failed to notify");
+            let notify = Command::new("notify-send")
+                .arg("Pomodoro complete! Nice work!")
+                .output()
+                .expect("failed to execute process");
+            let bepp = Command::new("aplay")
+                .arg("beep.wav")
+                .output()
+                .expect("failed to notify");
+        }
     }
 }
